b131e1fa496f50b4c2433725c5477d2f697cb9e1
Jean Pool Gutierrez <jean-gutierrez-117@hotmail.com>
2023-08-12 23:28:17 -0500

    2. Inicializando los scripts

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const passwordElement = document.getElementByI('password');
+const lengthTextElement = document.getElementById('passwordLength');
+const rangeElement = document.getElementById('range');
+const buttonGenerateElement = document.getElementById('generatePassword');
+
+
+const allowedCharacters = `abcdefghijklmñopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+-.,!'".$%&/()=?{}[]`;
+
